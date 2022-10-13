df513e515a0b6ef48080774b72d9c48f026652c6
Niels Tanis <nielstanis@live.nl>
2022-10-13 17:07:42 +1100

    Content

---
+++ Middle/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using iLib;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello Sydney 2022!");
+            Console.WriteLine($"ConsoleApp - {typeof(System.Net.Http.HttpClient).Assembly.Location}");
+            
+            var loadContext = new IsolatedLoadContext("../pub/lib.dll", new[]{ typeof(IProcessor) });
+            var d = loadContext.CreateInstance<IProcessor>();
+
+            var result = await d.ProcessDocumentAsync("Docs/schedule.pdf","Output/schedule2022.pdf");
+            //var result = await d.ProcessDocumentAsync("https://ndcporto.com/","Output/ndcporto.html");
+            
+            Console.WriteLine("Done...");
+        }
+    }
+}
